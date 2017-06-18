fedfb8a4935e3b0640c6f9fc69847c8566c77fae
MYunFeiYang <2728363142@qq.com>
2017-06-18 19:05:34 +0800

    第一次修改

--- main/person.js
+++ main/person.js
@@ -1,2 +1,13 @@
 // Write your code here
 module.exports = Person
+　function Point(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+Person.introduce = function () {
+  return 'My name is Tom. I am 21 years old.';
+};
+
+var p = new Point(Tom, 21);
+	p.introduce();
